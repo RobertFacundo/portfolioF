facefb82f41ccc7e73d67559907b215ae6e7dfaa
Facundo Robert <robert.facundo@hotmail.com>
2025-08-16 15:24:29 -0300

    Linkedin profile us

--- src/components/sidebar/Contact.jsx
+++ src/components/sidebar/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = ()=>{
             <a href="https://github.com/RobertFacundo" target="_blank" rel="noopener noreferrer">
                 <FaGithub/>
             </a>
-            <a href="https://www.linkedin.com/in/robertfacundodev/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/robertfacundodev/?locale=en_US" target="_blank" rel="noopener noreferrer">
                 <FaLinkedin/>
             </a>
             <a href="mailto:robertf.coder@gmail.com" target="_blank" rel="noopener noreferrer">
